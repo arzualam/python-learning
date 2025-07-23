0488897e5ce6d563ef4bd0fb5725339cd48f4164
Arzu Alam <40546860+arzualam@users.noreply.github.com>
2025-07-23 11:53:50 -0700

    Add files via upload

---
+++ addition-function.py
@@ -0,0 +1,25 @@
+# Defining addition function
+def addition(a, b):
+    return a + b
+
+# Main Program
+num1 = float(input('Enter 1st number:\n'))
+num2 = float(input('Enter 2nd number:\n'))
+
+# calling our function
+
+result = addition(num1, num2)
+print('The result is', result)
+
+# We can also add the main program as func and call the function to get the results
+
+def main()
+    num1 = float(input('Enter 1st number:\n'))
+    num2 = float(input('Enter 2nd number:\n'))
+
+    # calling our function
+
+    result = addition(num1, num2)
+    print('The result is', result)
+
+main()
---
+++ greetings-function.py
@@ -0,0 +1,18 @@
+# python function creation
+def greeting(name):
+    print('Hello', name)
+
+# Defining a function called greeting, which prints names but we need to call it
+
+# main body
+
+input_name = input('Enter your name:\n')
+
+greeting(input_name)
+
+# If we have multiple names
+
+name1 = input('Enter your name:\n')
+greeting(name1)
+name2 = input('Enter your name:\n')
+greeting(name2)
